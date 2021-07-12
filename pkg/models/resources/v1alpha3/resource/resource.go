7ecd5ea9c49c6168c490a187aa488ef0679aef19
rick <linuxsuren@users.noreply.github.com>
2021-07-12 20:48:26 +0800

    Import kubesphere devops plugin project

---
+++ pkg/models/resources/v1alpha3/resource/resource.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2020 The KubeSphere Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resource
+
+import (
+	"errors"
+
+	devopsv1alpha3 "devops.kubesphere.io/plugin/pkg/api/devops/v1alpha3"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+
+	"devops.kubesphere.io/plugin/pkg/api"
+	"devops.kubesphere.io/plugin/pkg/apiserver/query"
+	"devops.kubesphere.io/plugin/pkg/informers"
+	"devops.kubesphere.io/plugin/pkg/models/resources/v1alpha3"
+	"devops.kubesphere.io/plugin/pkg/models/resources/v1alpha3/devops"
+)
+
+var ErrResourceNotSupported = errors.New("resource is not supported")
+
+type ResourceGetter struct {
+	clusterResourceGetters    map[schema.GroupVersionResource]v1alpha3.Interface
+	namespacedResourceGetters map[schema.GroupVersionResource]v1alpha3.Interface
+}
+
+func NewResourceGetter(factory informers.InformerFactory, cache cache.Cache) *ResourceGetter {
+	namespacedResourceGetters := make(map[schema.GroupVersionResource]v1alpha3.Interface)
+	clusterResourceGetters := make(map[schema.GroupVersionResource]v1alpha3.Interface)
+
+	// kubesphere resources
+	clusterResourceGetters[devopsv1alpha3.GroupVersion.WithResource(devopsv1alpha3.ResourcePluralDevOpsProject)] = devops.New(factory.KubeSphereSharedInformerFactory())
+	return &ResourceGetter{
+		namespacedResourceGetters: namespacedResourceGetters,
+		clusterResourceGetters:    clusterResourceGetters,
+	}
+}
+
+// TryResource will retrieve a getter with resource name, it doesn't guarantee find resource with correct group version
+// need to refactor this use schema.GroupVersionResource
+func (r *ResourceGetter) TryResource(clusterScope bool, resource string) v1alpha3.Interface {
+	if clusterScope {
+		for k, v := range r.clusterResourceGetters {
+			if k.Resource == resource {
+				return v
+			}
+		}
+	}
+	for k, v := range r.namespacedResourceGetters {
+		if k.Resource == resource {
+			return v
+		}
+	}
+	return nil
+}
+
+func (r *ResourceGetter) Get(resource, namespace, name string) (runtime.Object, error) {
+	clusterScope := namespace == ""
+	getter := r.TryResource(clusterScope, resource)
+	if getter == nil {
+		return nil, ErrResourceNotSupported
+	}
+	return getter.Get(namespace, name)
+}
+
+func (r *ResourceGetter) List(resource, namespace string, query *query.Query) (*api.ListResult, error) {
+	clusterScope := namespace == ""
+	getter := r.TryResource(clusterScope, resource)
+	if getter == nil {
+		return nil, ErrResourceNotSupported
+	}
+	return getter.List(namespace, query)
+}
